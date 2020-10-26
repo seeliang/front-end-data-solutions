19704e5f94a715edc2a1691bee9a55360bb76179
seeliang <hi@sihuiliang.com>
2020-10-26 16:06:14 +1100

    emmbed react

--- solutions/redux-react-angular/react/index.js
+++ solutions/redux-react-angular/react/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('react')
-);
+
+
+const render = () => {
+  if(store.getState().message.text.length < 3) {
+    return
+  }
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('react')
+  );
+  unsubscribe();
+}
+
+const unsubscribe = store.subscribe(render)
